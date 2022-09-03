efe99571ce8cf3cd7c0c8065dd8c108607da40aa
Livingstone Maghanga <stoneriga@gmail.com>
2022-09-03 14:27:06 +0300

    setup of Firebase API keys

--- src/db.js
+++ src/db.js
@@ -3,6 +3,12 @@ import "firebase/database";
 
 const config = {
     // API KEYS
+  apiKey: "AIzaSyB2kGp07HgS_V9waamQCGjozXxzc6nHmTo",
+  authDomain: "novamessenger-39c8e.firebaseapp.com",
+  projectId: "novamessenger-39c8e",
+  storageBucket: "novamessenger-39c8e.appspot.com",
+  messagingSenderId: "248000963636",
+  appId: "1:248000963636:web:3558b03cc3b0dddbf1d661"
 }
 
 const db = firebase.initializeApp(config);
